3b53f2c9a9c98f3ba26df523e2f9c8d74b972639
Gerrit <g.pathuis@quicknet.nl>
2018-08-18 19:56:55 +0200

    First issue

---
+++ Excel_connect/Excel_Connect_folder/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FillWithStrings = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(25, 72)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(25, 101)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'FillWithStrings
+        '
+        Me.FillWithStrings.AutoSize = True
+        Me.FillWithStrings.Location = New System.Drawing.Point(109, 37)
+        Me.FillWithStrings.Name = "FillWithStrings"
+        Me.FillWithStrings.Size = New System.Drawing.Size(92, 17)
+        Me.FillWithStrings.TabIndex = 3
+        Me.FillWithStrings.Text = "FillWithStrings"
+        Me.FillWithStrings.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(303, 171)
+        Me.Controls.Add(Me.FillWithStrings)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Connect to Excel"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FillWithStrings As CheckBox
+End Class
---
+++ Excel_connect/Excel_Connect_folder/Form1.vb
@@ -0,0 +1,138 @@
+﻿Imports Microsoft.Office.Interop
+Public Class Form1
+    'Keep the application object and the workbook object global, so you can  
+    'retrieve the data in Button2_Click that was set in Button1_Click.
+    Dim objApp As Excel.Application
+    Dim objBook As Excel._Workbook
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object,
+      ByVal e As System.EventArgs) Handles Button1.Click
+        Dim objBooks As Excel.Workbooks
+        Dim objSheets As Excel.Sheets
+        Dim objSheet As Excel._Worksheet
+        Dim range As Excel.Range
+
+        ' Create a new instance of Excel and start a new workbook.
+        objApp = New Excel.Application()
+        objBooks = objApp.Workbooks
+        objBook = objBooks.Add
+        objSheets = objBook.Worksheets
+        objSheet = objSheets(1)
+
+        'Get the range where the starting cell has the address
+        'm_sStartingCell and its dimensions are m_iNumRows x m_iNumCols.
+        range = objSheet.Range("A1", Reflection.Missing.Value)
+        range = range.Resize(5, 5)
+
+        If (Me.FillWithStrings.Checked = False) Then
+            'Create an array.
+            Dim saRet(5, 5) As Double
+
+            'Fill the array.
+            Dim iRow As Long
+            Dim iCol As Long
+            For iRow = 0 To 5
+                For iCol = 0 To 5
+
+                    'Put a counter in the cell.
+                    saRet(iRow, iCol) = iRow * iCol
+                Next iCol
+            Next iRow
+
+            'Set the range value to the array.
+            range.Value = saRet
+
+        Else
+            'Create an array.
+            Dim saRet(5, 5) As String
+
+            'Fill the array.
+            Dim iRow As Long
+            Dim iCol As Long
+            For iRow = 0 To 5
+                For iCol = 0 To 5
+
+                    'Put the row and column address in the cell.
+                    saRet(iRow, iCol) = iRow.ToString() + "|" + iCol.ToString()
+                Next iCol
+            Next iRow
+
+            'Set the range value to the array.
+            range.Value = saRet
+        End If
+
+        'Return control of Excel to the user.
+        objApp.Visible = True
+        objApp.UserControl = True
+
+        'Clean up a little.
+        range = Nothing
+        objSheet = Nothing
+        objSheets = Nothing
+        objBooks = Nothing
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object,
+      ByVal e As System.EventArgs) Handles Button2.Click
+        Dim objSheets As Excel.Sheets
+        Dim objSheet As Excel._Worksheet
+        Dim range As Excel.Range
+
+        'Get a reference to the first sheet of the workbook.
+        On Error GoTo ExcelNotRunning
+        objSheets = objBook.Worksheets
+        objSheet = objSheets(1)
+
+ExcelNotRunning:
+        If (Not (Err.Number = 0)) Then
+            MessageBox.Show("Cannot find the Excel workbook.  Try clicking Button1 to " +
+            "create an Excel workbook with data before running Button2.",
+            "Missing Workbook?")
+
+            'We cannot automate Excel if we cannot find the data we created, 
+            'so leave the subroutine.
+            Exit Sub
+        End If
+
+        'Get a range of data.
+        range = objSheet.Range("A1", "E5")
+
+        'Retrieve the data from the range.
+        Dim saRet(,) As Object
+        saRet = range.Value
+
+        'Determine the dimensions of the array.
+        Dim iRows As Long
+        Dim iCols As Long
+        iRows = saRet.GetUpperBound(0)
+        iCols = saRet.GetUpperBound(1)
+
+        'Build a string that contains the data of the array.
+        Dim valueString As String
+        valueString = "Array Data" + vbCrLf
+
+        Dim rowCounter As Long
+        Dim colCounter As Long
+        For rowCounter = 1 To iRows
+            For colCounter = 1 To iCols
+
+                'Write the next value into the string.
+                valueString = String.Concat(valueString,
+                    saRet(rowCounter, colCounter).ToString() + ", ")
+
+            Next colCounter
+
+            'Write in a new line.
+            valueString = String.Concat(valueString, vbCrLf)
+        Next rowCounter
+
+        'Report the value of the array.
+        MessageBox.Show(valueString, "Array Values")
+
+        'Clean up a little.
+        range = Nothing
+        objSheet = Nothing
+        objSheets = Nothing
+    End Sub
+End Class
