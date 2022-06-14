8f3b41c59f69ab59f2ca45a0f1cfba796ea5523f
GerritPathuis <GerritPathuis@users.noreply.github.com>
2022-06-14 11:51:16 +0200

    Update

--- Excel_connect/Excel_Connect_folder/Form1.Designer.vb
+++ Excel_connect/Excel_Connect_folder/Form1.Designer.vb
@@ -24,39 +24,86 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(25, 72)
+        Me.Button1.Location = New System.Drawing.Point(25, 36)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(139, 36)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Save excel file"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
         Me.Button2.Location = New System.Drawing.Point(25, 101)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.Size = New System.Drawing.Size(139, 42)
         Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "Retrieve excel file"
         Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(25, 194)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(139, 42)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "DGV_to_file"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3})
+        Me.DataGridView1.Location = New System.Drawing.Point(268, 72)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(288, 201)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Column1"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Column2"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Column3"
+        Me.Column3.Name = "Column3"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(303, 171)
+        Me.ClientSize = New System.Drawing.Size(608, 334)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Connect to Excel"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
 End Class
--- Excel_connect/Excel_Connect_folder/Form1.vb
+++ Excel_connect/Excel_Connect_folder/Form1.vb
@@ -2,14 +2,44 @@
 '
 'See https://support.microsoft.com/en-us/help/302094/how-to-automate-excel-from-visual-basic-net-to-fill-or-to-obtain-data
 '
+
+
 Public Class Form1
     'Keep the application object and the workbook object global, so you can  
     'retrieve the data in Button2_Click that was set in Button1_Click.
     Dim objApp As Excel.Application
     Dim objBook As Excel._Workbook
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With DataGridView1
+            .ColumnCount = 3
+            .Rows.Clear()
+            .Rows.Add(4)
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+
+            .Columns(0).HeaderText = "H1"
+            .Columns(1).HeaderText = "H2"
+            .Columns(2).HeaderText = "H3"
+
+            For i = 0 To 3
+                .Rows(i).Cells(0).Value = "00"
+                .Rows(i).Cells(1).Value = "01"
+                .Rows(i).Cells(2).Value = "02"
+            Next
+        End With
+    End Sub
+
+
     Private Sub Button1_Click(ByVal sender As System.Object,
       ByVal e As System.EventArgs) Handles Button1.Click
+        Save_excel_file()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Retrieve_file()
+    End Sub
+
+    Private Sub Save_excel_file()
         Dim objBooks As Excel.Workbooks
         Dim objSheets As Excel.Sheets
         Dim objSheet As Excel._Worksheet
@@ -27,23 +57,21 @@ Public Class Form1
         range = objSheet.Range("A1", Reflection.Missing.Value)
         range = range.Resize(5, 1)
 
-
         'Create an array.
         Dim saRet(5, 1) As String
 
         'Fill the array.
         Dim iRow As Long
-            Dim iCol As Long
-            For iRow = 0 To 5
+        Dim iCol As Long
+        For iRow = 0 To 5
             For iCol = 0 To 1
                 'Put the row and column address in the cell.
-                saRet(iRow, iCol) = iRow.ToString() + "|" + iCol.ToString()
+                saRet(iRow, iCol) = iRow.ToString() & "|" & iCol.ToString()
             Next iCol
         Next iRow
 
-            'Set the range value to the array.
-            range.Value = saRet
-
+        'Set the range value to the array.
+        range.Value = saRet
 
         'Return control of Excel to the user.
         objApp.Visible = True
@@ -56,32 +84,31 @@ Public Class Form1
         objBooks = Nothing
     End Sub
 
-    Private Sub Button2_Click(ByVal sender As System.Object,
-      ByVal e As System.EventArgs) Handles Button2.Click
+    Private Sub Retrieve_file()
         Dim objSheets As Excel.Sheets
         Dim objSheet As Excel._Worksheet
         Dim range As Excel.Range
 
         'Get a reference to the first sheet of the workbook.
-        On Error GoTo ExcelNotRunning
+
         objSheets = objBook.Worksheets
         objSheet = objSheets(1)
-ExcelNotRunning:
-        If (Not (Err.Number = 0)) Then
-            MessageBox.Show("Cannot find the Excel workbook.  Try clicking Button1 to " +
-            "create an Excel workbook with data before running Button2.",
-            "Missing Workbook?")
 
-            'We cannot automate Excel if we cannot find the data we created, 
-            'so leave the subroutine.
-            Exit Sub
-        End If
+        'If (Not (Err.Number = 0)) Then
+        '    MessageBox.Show("Cannot find the Excel workbook.  Try clicking Button1 to " &
+        '    "create an Excel workbook with data before running Button2.",
+        '    "Missing Workbook?")
+
+        '    'We cannot automate Excel if we cannot find the data we created, 
+        '    'so leave the subroutine.
+
+        'End If
 
         'Get a range of data.
         range = objSheet.Range("A1", "E5")
 
         'Retrieve the data from the range.
-        Dim saRet(,) As Object
+        Dim saRet As Object(,)
         saRet = range.Value
 
         'Determine the dimensions of the array.
@@ -92,7 +119,7 @@ ExcelNotRunning:
 
         'Build a string that contains the data of the array.
         Dim valueString As String
-        valueString = "Array Data" + vbCrLf
+        valueString = "Array Data" & vbCrLf
 
         Dim rowCounter As Long
         Dim colCounter As Long
@@ -101,7 +128,7 @@ ExcelNotRunning:
 
                 'Write the next value into the string.
                 valueString = String.Concat(valueString,
-                    saRet(rowCounter, colCounter).ToString() + ", ")
+                    saRet(rowCounter, colCounter).ToString() & ", ")
 
             Next colCounter
 
@@ -117,4 +144,34 @@ ExcelNotRunning:
         objSheet = Nothing
         objSheets = Nothing
     End Sub
+
+    Private Sub DGV_to_file()
+        Dim xlApp As Excel.Application
+        Dim xlBook As Excel.Workbook
+        Dim xlSheet As Excel.Worksheet
+
+        xlApp = New Excel.Application
+        xlBook = xlApp.Workbooks.Open(Filename:="C:\Repos\MS-excel-connection\Excel_connect\Typical_PSD.xlsx", IgnoreReadOnlyRecommended:=True, ReadOnly:=False, Editable:=True)
+        xlSheet = xlBook.Worksheets(1)
+        If DataGridView1.DataSource IsNot Nothing Then
+            Dim i, j As Integer
+            For i = 1 To DataGridView1.RowCount - 1
+                For j = 1 To DataGridView1.ColumnCount
+                    xlSheet.Cells(i + 1, j) = DataGridView1.Rows(i - 1).Cells(j - 1).Value
+                Next
+            Next
+            xlApp.Visible = True
+            xlApp.UserControl = True
+            xlApp.Quit()
+            xlApp = Nothing
+        Else
+            MsgBox("Le tableau est vide")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        DGV_to_file()
+    End Sub
+
+
 End Class
